f79f961e60665fe5599401f3e73e3429b7f7fd05
Nabihah Sema <st10440590@vcconnect.edu.za>
2025-06-27 09:31:09 +0200

    added dictionaries

--- test1-Porg part 3/Form1.cs
+++ test1-Porg part 3/Form1.cs
@@ -13,6 +13,28 @@ namespace test1_Porg_part_3
     public partial class Form1: Form
     {
 
+        Dictionary<string, string> responses = new Dictionary<string, string>
+        {
+            { "how are you", "I'm doing great! How can I assist you with cybersecurity today?" },
+            { "what's your purpose", "My purpose is to educate you on how to create a safe environment online by giving cybersecurity tips." +
+                  "\nI can answer questions regarding phishing emails, password safety and safe browsing."},
+            { "help", "You can ask about passwords, phishing, safe browsing, or general cybersecurity guidance." }
+        };
+        Dictionary<string, List<string>> keywordGroups = new Dictionary<string, List<string>>()
+        {
+            {"password", new List<string> {"passwords", "password", "strong password", "secure password"} },
+            {"phishing", new List<string> {"phishing", "phishing email", "fake email", "scam", "scams"} },
+            {"safe browsing", new List<string> {"browse safely", "secure browsing", "browsing","privacy", "safe-browsing"} },
+             {"how are you", new List <string> {"how are u", "how r u", "how are you?", "how r u?"} },
+                {"what's your purpose", new List <string> {"purpose", "what is your purpose", "whats your purpose" } },
+        };
+
+        //dictionary to store a phrases of confused words 
+        public static string lastTopic = " ";
+        static List<string> confusedWords = new List<string>
+        {
+            "i do not understand", "what", "explain", "dont understand", "don't understand", "huh", "confused", "lost", "more detail", "more info", "more information", "explain in more detail"
+        };
 
 
 
